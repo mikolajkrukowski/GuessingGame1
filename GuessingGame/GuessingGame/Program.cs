030e86ea0118070bfe2495e2abfcf0b346ff0db6
Mikołaj Krukowski <mikolajk12@interia.pl>
2024-09-12 22:38:42 +0200

    Simple number guessing game

--- GuessingGame/GuessingGame/Program.cs
+++ GuessingGame/GuessingGame/Program.cs
@@ -7,7 +7,7 @@ namespace GuessingGame
         static void Main(string[] args)
         {
             Random random = new Random();
-            int number = random.Next(50) + 1;
+            int number = random.Next(30) + 1;
             int guessNumber = 0;
             int tries = 0;
 
@@ -15,20 +15,20 @@ namespace GuessingGame
             {
                 tries++;
 
-                Console.Write("Guess the number between 1 and 50. You have 10 chances.");
+                Console.Write("Guess the number between 1 and 30. \n");
                 int.TryParse(Console.ReadLine(), out guessNumber);
 
                 if (guessNumber > number)
                 {
-                    Console.WriteLine("{0} is too high!", guessNumber);
+                    Console.WriteLine("{0} is too high! \n", guessNumber);
                 }
                 else if (guessNumber < number)
                 {
-                    Console.WriteLine("{0} is too low!", guessNumber);
+                    Console.WriteLine("{0} is too low! \n", guessNumber);
                 }
                 else
                 {
-                    Console.WriteLine("{0} is correct! GZ. Want to play again?", guessNumber);
+                    Console.WriteLine("{0} is correct! GZ. Want to play again? \n", guessNumber);
                     Console.WriteLine("Number of tries: {0}", tries);
                 }
             }
